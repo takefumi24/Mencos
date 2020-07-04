d55e1b2c7afcdae50e28df229ff5dfe40ae2732f
takefumi24 <t_ohba24@yahoo.co.jp>
2020-07-04 19:24:32 +0900

    post column

--- db/migrate/20200704101952_create_posts.rb
+++ db/migrate/20200704101952_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-
+      t.string    :name
+      t.integer   :price
+      t.string    :shop
+      t.text      :remark
       t.timestamps
     end
   end
