f6a061fa8a8e8fd0cece9a7074e977b1034600a4
takefumi24 <t_ohba24@yahoo.co.jp>
2020-07-22 01:39:22 +0900

    add_brand-column

--- db/migrate/20200704101952_create_posts.rb
+++ db/migrate/20200704101952_create_posts.rb
@@ -2,6 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.string    :name
+      t.string    :brand
       t.integer   :price
       t.string    :shop
       t.text      :remark
